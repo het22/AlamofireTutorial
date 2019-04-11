65cdc88dd6d25a441946357950aed39af6b805ea
het99 <gocks0922@gmail.com>
2019-04-11 22:59:39 +0900

    Fix Response Handler Type

--- AlamofireTutorial/ViewController.swift
+++ AlamofireTutorial/ViewController.swift
@@ -59,7 +59,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             self?.activityIndicatorView.isHidden = true
             self?.activityIndicatorView.stopAnimating()
             
-            // 10. 태그값 업데이트하고 테이블뷰 리로드
+            // 9. 태그값 업데이트하고 테이블뷰 리로드
             self?.tags = tags ?? []
             self?.tableView.reloadData()
         }
@@ -73,7 +73,6 @@ extension ViewController {
     
     func upload(image: UIImage,
                 completion: @escaping (_ tags: [String]?) -> Void) {
-        
         // 3. 이미지를 데이터로 변환
         guard let imageData = image.jpegData(compressionQuality: 0.5) else {
             print("Could not get JPEG representation of UIImage")
@@ -82,39 +81,37 @@ extension ViewController {
         
         // 4. 업로드 요청
         Alamofire.upload(multipartFormData: { multipartFormData in
-            multipartFormData.append(imageData,
-                                     withName: "imagefile",
-                                     fileName: "image.jpg",
-                                     mimeType: "image/jpeg")
-        },
+                            multipartFormData.append(imageData,
+                                                     withName: "imagefile",
+                                                     fileName: "image.jpg",
+                                                     mimeType: "image/jpeg")
+                        },
                          with: ImaggaRouter.content,
                          encodingCompletion: { encodingResult in
                             switch encodingResult {
                             case .success(let request, _, _):
                                 request
                                     .validate()
-                                    .responseJSON(completionHandler: { response in
-
-                                        guard response.result.isSuccess, let value = response.result.value else {
+                                    .responseData { response in
+                                        guard response.result.isSuccess, let data = response.data else {
                                             print("Error while uploading file: \(String(describing: response.result.error))")
                                             completion(nil)
                                             return
                                         }
                                         
                                         do {
-                                            // 5. 결과값(Any) json 파싱
-                                            let data = try JSONSerialization.data(withJSONObject: value)
-                                            // 6. json을 Codable 객체로 디코드
-                                            let uploadRes = try JSONDecoder().decode(UploadRes.self, from: data)
-                                            // 7. 업로드 된 이미지의 id값으로 태그 다운로드 요청
-                                            self.downloadTags(contentID: uploadRes.uploaded[0].id, completion: { completion($0) })
+                                            // 5. json을 Codable 객체로 디코드
+                                            let json = try JSONDecoder().decode(UploadRes.self, from: data)
+                                            // 6. 업로드 된 이미지의 id값으로 태그 다운로드 요청
+                                            self.downloadTags(contentID: json.uploaded[0].id, completion: { completion($0) })
                                         } catch let err {
                                             print(err)
                                             completion(nil)
                                         }
-                                    })
+                                    }
                             case .failure(let encodingError):
                                 print(encodingError)
+                                completion(nil)
                             }
         })
         
@@ -122,28 +119,27 @@ extension ViewController {
     
     func downloadTags(contentID: String, completion: @escaping ([String]?)->Void) {
         
-        // 8. 다운로드 요청
+        // 7. 다운로드 요청
         Alamofire
             .request(ImaggaRouter.tags(contentID))
-            .responseJSON(completionHandler: { response in
-                
-                guard response.result.isSuccess, let value = response.result.value else {
+            .validate()
+            .responseData { response in
+                guard response.result.isSuccess, let data = response.data else {
                     print("Error while fetching tags: \(String(describing: response.result.error))")
                     completion(nil)
                     return
                 }
                 
                 do {
-                    let data = try JSONSerialization.data(withJSONObject: value)
-                    let tagResponse = try JSONDecoder().decode(TagResponse.self, from: data)
-                    // 9. 태그 값 매핑해서 핸들러에 넘김
-                    let tags = tagResponse.results[0].tags.compactMap({return $0.tag})
+                    let json = try JSONDecoder().decode(TagResponse.self, from: data)
+                    // 8. 태그 값 매핑해서 핸들러에 넘김
+                    let tags = json.results[0].tags.compactMap({return $0.tag})
                     completion(tags)
                 } catch let err {
                     print(err)
                     completion(nil)
                 }
-            })
+            }
     }
 }
 
